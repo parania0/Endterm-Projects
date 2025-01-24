9144030fb9f71d72dd5eded4a908843cb0a4a4c8
Yerkegali <147907433+parania0@users.noreply.github.com>
2025-01-24 09:48:19 +0500

    Update Quiz.java

--- src/models/Quiz.java
+++ src/models/Quiz.java
@@ -1,5 +1,8 @@
 package models;
+public class Quiz extends Question {
 
-public class Quiz {
+    public Quiz(String text, String option1, String option2, String option3, String rightanswer) {
+        super(text, option1, option2, option3, rightanswer);
+    }
 
 }
