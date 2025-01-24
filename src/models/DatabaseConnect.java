035b2b6df5c9dd7a92dba007df83fd41a2fec7e3
Yerkegali <147907433+parania0@users.noreply.github.com>
2025-01-24 09:47:33 +0500

    Update DatabaseConnect.java

--- src/models/DatabaseConnect.java
+++ src/models/DatabaseConnect.java
@@ -1,4 +1,8 @@
 package models;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 public class DatabaseConnect {
+    private static final String URL = "jdbc:postgresql://localhost:5432/simpledb";
+    private static final String PASSWORD = "0000";
 }
